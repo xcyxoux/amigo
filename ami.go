95612d2eaf37feba2109ab4f916d5e7a43666347
Pavel Sukhodoev <xcyxoux@gmail.com>
2020-03-25 19:31:48 +0300

    possible solution

--- ami.go
+++ ami.go
@@ -388,9 +388,12 @@ func (a *amiAdapter) reader(conn net.Conn, stop <-chan struct{}, readErrChan cha
 			var event map[string]string
 			var err error
 			event, err = readMessage(bufReader)
-			if err != nil {
+			if err != nil && err != io.EOF {
 				chanErr <- err
 				return
+			}
+			if err == io.EOF {
+				continue
 			}
 
 			event["#"] = strconv.Itoa(i)
